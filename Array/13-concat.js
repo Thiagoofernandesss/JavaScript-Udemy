824fa61daf96bb369133a756c4696547d998c945
Thiagofernandesss <thiago.f.l.costa@hotmail.com>
2023-04-21 19:02:08 -0300

    atualizando

--- Array/13-concat.js
+++
@@ -1,7 +0,0 @@
-const filhas = ['Ana','Bia']
-const filhos = ['Thiago','Léo']
-const todos = filhas.concat(filhos,'Rodrigo')
-
-console.log(todos)
-console.log(filhas)
-console.log(filhos)
--- node/moduloA.js
+++
@@ -1,3 +0,0 @@
-this.ola = 'Fala pessoal'
-exports.bemVindo = 'Bem vindo ao node'
-module.exports.ateLogo = 'Até o próximo exemplo'
